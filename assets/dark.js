7602eed76b3f41e35f6d5b92778e4d0eb8019c90
Ac_K <Acoustik666@gmail.com>
2026-01-19 22:38:07 +0100

    Delete assets directory

--- assets/dark.js
+++
@@ -1,8 +0,0 @@
-function toggleTheme() {
-  const html = document.documentElement;
-  html.dataset.theme = html.dataset.theme === 'dark' ? 'light' : 'dark';
-}
-
-if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-  document.documentElement.dataset.theme = 'dark';
-}
